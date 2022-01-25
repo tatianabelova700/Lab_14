c882d2c6f8cf459d352961e2f73267d557b9716d
Tatyana <Tatyana@DESKTOP-74DNP3C>
2022-01-25 20:00:40 +0300

    Добавьте файлы проекта.

---
+++ Lab_14/Animal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_14
+{
+    abstract class Animal
+    {
+        public abstract string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public abstract void Say();
+        public void ShowInfo()
+        {
+            Console.WriteLine(Name);
+            Say();
+        }
+    }
+    class Cat : Animal
+    {
+        string say;
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Cat(string name, string say)
+            : base(name)
+
+        {
+            this.say = say;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Мяу");
+        }
+
+    }
+
+    class Dog : Animal
+    {
+         string say;
+         string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dog(string name, string say)
+            : base(name)
+
+        {
+            this.say = say;
+        }
+        public override void Say()
+        {
+            Console.WriteLine("Гав-гав");
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}
---
+++ Lab_14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_14
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Cat cat = new Cat("Милош", "Мяу");
+            cat.ShowInfo();
+
+            Dog dog = new Dog("Бим", "Гав - гав");
+            dog.ShowInfo();
+            Console.ReadKey();
+        }
+        
+    }
+}
